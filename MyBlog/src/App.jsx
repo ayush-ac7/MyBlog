7e98eb7b3a980df75818cada43efd3958abed66f
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-04-26 16:57:58 +0530

    inital commit

---
+++ MyBlog/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-black h-screen">
+      <h1 className="text-white font-bold text-2xl">MyBlog</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ MyBlog/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <div className="bg-black h-screen">
+      <h1 className="text-white font-bold text-2xl">MyBlog</h1>
+      </div>
+    )
+}
