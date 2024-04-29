46f0a3fb4ac4af2f2098545f81c1be7fe96f752a
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-04-29 18:40:12 +0530

    added outlet for children pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="">
-    <Header />
+    <div>
+      <Header />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
 export default App;
