f47ca5bd503292cc3ea03ab39d7af19543ac0887
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-04-29 19:18:32 +0530

    added key for elements

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -7,19 +7,23 @@ const BlogCard = ({ blogs }) => {
   return (
     <div className="cards py-6 px-6 flex flex-wrap max-w-6xl">
       {blogs.map((blog, i) => (
-        <div className="card w-[300px] h-[320px] border-gray-200 border-2 p-2 rounded-lg shadow-lg">
-          <div>
-            <img className="w-70 rounded-lg pb-1" src={blog.image} />
+        <Link key={i}>
+          <div className="card w-[300px] h-[320px] border-gray-200 border-2 p-2 rounded-lg shadow-lg">
+            <div>
+              <img className="w-70 rounded-lg pb-1" src={blog.image} />
+            </div>
+            <p className="font-semibold pb-1 hover:text-blue-500">
+              {blog.title}
+            </p>
+            <div className="flex items-center pb-1">
+              <span className="pr-1">
+                <FaUserPen />
+              </span>
+              <span>{blog.author}</span>
+            </div>
+            <p className="text-sm ">{blog.published_date}</p>
           </div>
-          <p className="font-semibold pb-1 hover:text-blue-500">{blog.title}</p>
-          <div className="flex items-center pb-1">
-            <span className="pr-1">
-              <FaUserPen />
-            </span>
-            <span>{blog.author}</span>
-          </div>
-          <p className="text-sm ">{blog.published_date}</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
