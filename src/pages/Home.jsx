779e12d383fdae31de452fd926a2dc200a48405f
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-04-30 21:26:57 +0530

    made a write a blog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BlogCard from "../components/BlogCard";
 import BlogsData from "../utils/BlogsData.json";
+import Banner from "../components/Banner";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,9 @@ const Home = () => {
   return (
     <div>
       <div className="w-full">
+        <div>
+        <Banner />
+        </div>
         <div className="links max-w-7xl mx-auto mt-5 border-gray-200 border-b-2 pb-4">
           {["All", "Startups", "Security", "AI", "Apps", "Tech"].map(
             (item, i) => (
@@ -25,8 +29,37 @@ const Home = () => {
           )}
         </div>
       </div>
-      <div className="cards">
-        <BlogCard blogs={blogs} />
+
+      <div className="flex px-4">
+        <div className="cards">
+          <BlogCard blogs={blogs} />
+        </div>
+        <form>
+          <div className="write-blog text-center p-2 my-4">
+            <div>
+              <h1 className="py-4 font-bold text-xl text-orange-500">Write your Blog!</h1>
+
+              <input
+                type="text"
+                placeholder="Blog Title"
+                className="border-gray-300 rounded-md border-[2px] my-2"
+              />
+              <textarea
+                className="w-[350px] h-[200px] border-[2px] rounded-lg border-gray-300 my-2"
+                type="text"
+                placeholder="Start your blog..."
+              />
+              <input
+                type="text"
+                placeholder="Author Name"
+                className="border-gray-300 rounded-md border-[2px] "
+              />
+            </div>
+            <button className="bg-blue-500 py-1 rounded-2xl px-6 m-4 text-white">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
