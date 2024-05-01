8e1ae59813381972e862706990fcefcb0a4b7448
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-05-01 19:57:06 +0530

    created local api with express

---
+++ index.cjs
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blog = require("./src/utils/BlogsData.json");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("blog server is running");
+});
+
+app.get("/blog", (req, res) => {
+    
+  res.send(blog);
+  
+});
+
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
